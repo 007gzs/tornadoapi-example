1c506de5e054b5ba4917b116e77cbf3d0d8e12d2
007gzs <007gzs@gmail.com>
2018-11-06 10:05:17 +0800

    raw_body description remark

--- api/handlers.py
+++ api/handlers.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals
 from tornadoapi import fields
 from tornadoapi.conf import settings
 from tornadoapi.core.err_code import ErrCode
+from tornadoapi.core.exceptions import CustomError
 from tornadoapi.handler import ApiHandler, ApiDocHandler
 
 
@@ -15,6 +16,7 @@ class BaseHandler(ApiHandler):
 
 class TestHandler(BaseHandler):
     test_param = fields.CharField(description='测试参数', default=None)
+    test_body = fields.JSONField(description='请求体测试', required=False, raw_body=True)
     test_choice = fields.ChoiceField(description='选择参数', default=None, choices=((0, '选项0'), (1, '选项1')))
 
     @classmethod
@@ -25,13 +27,31 @@ class TestHandler(BaseHandler):
     def get_handler_name(cls):
         return '测试'
 
+    @classmethod
+    def get_handler_remark(cls):
+        return '测试 备注'
+
+    @classmethod
+    def get_handler_description(cls):
+        return '测试 描述'
+
     def get(self, *args, **kwargs):
+        action = None
+        if self.test_body and isinstance(self.test_body, dict):
+            if 'action' not in self.test_body:
+                raise CustomError(ErrCode.ERR_ACTION_NOT_FOUND)
+            else:
+                action = self.test_body['action']
+
         ret = {
             'test_param': self.test_param,
             'test_choice': self.test_choice,
+            'body_action': action,
             'title': settings.TITLE
         }
         self.write_api(ret)
+
+    post = get
 
 
 default_handlers = [
--- config/settings.py
+++ config/settings.py
@@ -9,3 +9,7 @@ from config.local_settings import *  # NOQA
 DEBUG = os.environ.get('IS_DEBUG', '1') != '0'
 
 TITLE = 'test'
+
+ERROR_CODE_DEFINE = (
+    ('ERR_ACTION_NOT_FOUND',            10001,  '未找到 action '),
+)
